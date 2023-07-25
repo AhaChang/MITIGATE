732e9a780f68b0e5bc6edaacd077edc4bcaf7e86
AhaChang <wenjing1336@gmail.com>
2023-07-25 11:40:59 +0000

    add nc & ad model

--- models.py
+++ models.py
@@ -78,14 +78,43 @@ class MultiTask(nn.Module):
 
         self.encoder = Encoder(nfeat, nhid, dropout)
         self.dis_neighbor = Discriminator(nhid, nout, dropout)
-        self.dis_anomlay = Discriminator(nhid, 2, dropout) 
+        self.dis_anomaly = Discriminator(nhid, 2, dropout) 
 
     def forward(self, x, adj):
         x = self.encoder(x, adj)
         pred_nc = self.dis_neighbor(x)
-        pred_ad = self.dis_anomlay(x)
+        pred_ad = self.dis_anomaly(x)
         return x, pred_nc, pred_ad
-    
+
+
+class NCModel(nn.Module):
+    def __init__(self, nfeat, nhid, nout, dropout) -> None:
+        super(NCModel, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nout)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        embed = self.gc1(x, adj)
+        x = F.dropout(F.relu(embed), self.dropout, training=self.training)
+        x = F.relu(self.gc2(x, adj))
+        return embed, x
+
+
+class ADModel(nn.Module):
+    def __init__(self, nfeat, nhid, nout, dropout) -> None:
+        super(ADModel, self).__init__()
+
+        self.encoder = Encoder(nfeat, nhid, dropout)
+        self.discriminator = Discriminator(nhid, nout, dropout)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        embed = self.encoder(x, adj)
+        pred = self.discriminator(embed)
+        return embed, pred
+
 
 class SingleTask(nn.Module):
     def __init__(self, nfeat, nhid, nout, dropout):
@@ -98,3 +127,15 @@ class SingleTask(nn.Module):
         x = self.encoder(x, adj)
         pred = self.discriminator(x)
         return x, pred
+    
+
+class LinearSelector(nn.Module):
+    def __init__(self, nfeat, nhid) -> None:
+        super(LinearSelector, self).__init__()
+        self.fc1 = nn.Linear(nfeat, nhid)
+        self.fc2 = nn.Linear(nhid, 2)
+    
+    def forward(self, feats):
+        x = self.fc1(feats)
+        x = self.fc2(F.relu(x))
+        return x
